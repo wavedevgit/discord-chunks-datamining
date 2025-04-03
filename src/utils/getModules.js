58bf0fa523dd73a7cd0a7fa1b5b4ff9d8599031a
Wave Dev <66224387+wavedevgit@users.noreply.github.com>
2025-04-03 19:53:31 +0000

    Update getModules.js

--- src/utils/getModules.js
+++ src/utils/getModules.js
@@ -1,42 +1,43 @@
-const acorn = require('acorn');
-const walk = require('acorn-walk');
+const acorn = require("acorn");
+const walk = require("acorn-walk");
 
 function getModules(code) {
-    const ast = acorn.parse(code, { ecmaVersion: 'latest' });
-    const result = { js: {}, css: {} };
-    walk.simple(ast, {
-        ExpressionStatement(node) {
-            // js function
-            if (
-                node.expression?.left?.object?.name === '__webpack_require__' &&
-                node.expression?.right?.type === 'FunctionExpression'
-            ) {
-                let type;
-                const add = () => {
-                    walk.simple(node.expression.right.body, {
-                        ObjectExpression(node) {
-                            for (let prop of node.properties) {
-                                result[type][prop.key.value] = prop.value.value;
-                            }
-                        },
-                    });
-                };
-                switch (node.expression.left.property.name) {
-                    case 'k': {
-                        type = 'css';
-                        add();
-                        return;
-                    }
-                    case 'u': {
-                        type = 'js';
-                        add();
-                        return;
-                    }
-                }
-            }
-        },
-    });
-    return result;
+  const ast = acorn.parse(code, { ecmaVersion: "latest" });
+  const result = { js: {}, css: {} };
+  walk.simple(ast, {
+    ExpressionStatement(node) {
+      // js function
+      if (
+        (node.expression?.left?.object?.name === "__webpack_require__" &&
+          node.expression?.right?.type === "FunctionExpression") ||
+        node.expression?.right?.type === "ArrowFunctionExpression"
+      ) {
+        let type;
+        const add = () => {
+          walk.simple(node.expression.right.body, {
+            ObjectExpression(node) {
+              for (let prop of node.properties) {
+                result[type][prop.key.value] = prop.value.value;
+              }
+            },
+          });
+        };
+        switch (node?.expression?.right?.body?.right?.value) {
+          case ".js": {
+            type = "js";
+            add();
+            return;
+          }
+          case ".css": {
+            type = "css";
+            add();
+            return;
+          }
+        }
+      }
+    },
+  });
+  return result;
 }
 
 module.exports = getModules;
